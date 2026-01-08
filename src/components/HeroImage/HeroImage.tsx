5a5dfc4aaa04dd16c545c215daed565c7a2c854d
Denys Koval <metalheadden@gmail.com>
2026-01-08 14:04:16 +0200

    hero img url fix

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,6 +1,6 @@
 const HeroImage = () => {
   return (
-    <div className="block w-142 h-132.5 rounded-[30px] bg-[url(./hero@1x.webp)] bg-top"></div>
+    <div className="block w-142 h-132.5 rounded-[30px] bg-[url(/hero@1x.webp)] bg-top"></div>
   );
 };
 
